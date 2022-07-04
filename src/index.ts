aa32db12fd4668b2f5d39d29ecd59e9dd9db45f8
icy <blakckk@gmail.com>
2022-07-04 05:48:33 +0200

    Error handling and other middlewares added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello");
+import express, { Application, Request, Response } from "express";
+import helmet from "helmet";
+import errorMiddleware from "./middlewares/errors";
+const PORT = 3000;
+const app: Application = express();
+
+app.use(express.json());
+app.use(helmet());
+app.get("/", (req: Request, res: Response) => {
+  res.send("I am tired");
+});
+app.use(errorMiddleware);
+app.use((_req: Request, res: Response) => {
+  res
+    .status(404)
+    .send("Error: The route you're trying to access doesn't exist");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+export default app;
---
+++ src/middlewares/errors.ts
@@ -0,0 +1,18 @@
+import { Response, Request, NextFunction } from "express";
+interface Error {
+  name?: string;
+  stack?: string;
+  message?: string;
+  status?: number;
+}
+const errorMiddleware = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const status = error.status || 500;
+  const message = error.message || "An error occured.";
+  res.status(status).json({ status, message });
+};
+export default errorMiddleware;

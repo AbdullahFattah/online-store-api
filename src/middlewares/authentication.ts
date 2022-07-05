d61369456fa16f828ce69f2290a08966ea8131f8
icy <blakckk@gmail.com>
2022-07-05 07:38:37 +0200

    Authentication added for required routes

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import error from "./errors";
+import jwt from "jsonwebtoken";
+
+const unauthorizedError = (next: NextFunction) => {
+  const error = new Error("Unable to log in, try again later");
+  next(error);
+};
+
+const validateTokenMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.get("Authorization");
+
+    if (authHeader) {
+      const bearer = authHeader.split(" ")[0];
+      const token = authHeader.split(" ")[1];
+      if (token && bearer === "Bearer") {
+        const decode = jwt.verify(
+          token,
+          process.env.TOKEN_SECRET as unknown as string
+        );
+        if (decode) {
+          next();
+        } else {
+          unauthorizedError(next);
+        }
+      } else {
+        unauthorizedError(next);
+      }
+    } else {
+      unauthorizedError(next);
+    }
+  } catch (error) {
+    unauthorizedError(next);
+  }
+};
+export default validateTokenMiddleware;
--- src/routes/api/users_route.ts
+++ src/routes/api/users_route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import * as handlers from "../../handlers/users_handler";
+import authenticationMiddleware from "../../middlewares/authentication";
 const routes = express.Router();
 
 routes.post("/", handlers.create);
-routes.get("/", handlers.getUsers);
-routes.get("/:id", handlers.getUser);
-routes.patch("/:id", handlers.updateUser);
-routes.delete("/:id", handlers.deleteUser);
+routes.get("/", authenticationMiddleware, handlers.getUsers);
+routes.get("/:id", authenticationMiddleware, handlers.getUser);
+routes.patch("/:id", authenticationMiddleware, handlers.updateUser);
+routes.delete("/:id", authenticationMiddleware, handlers.deleteUser);
 routes.post("/authenticate", handlers.authenticate);
 
 export default routes;
